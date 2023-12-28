3c5beb82500335b381f6c760da558a3b788b0a8e
NitenSapkota175 <nitensapkota1705@gmail.com>
2023-12-29 00:38:38 +0530

    Module 2 completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 const child1 = React.createElement('p',{},'i am child1')
 const child2 = React.createElement('p',{},'i am child2')
-//const div = <div className="text">Hello 2</div>
-const div = React.createElement('div',{className:'text'},[child1,child2])
+const div = <div className="text">Hello 2</div>
+// const div = React.createElement('div',{className:'text'},[child1,child2])
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(div)
